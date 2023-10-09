b08020810638b654d489c64718fe10a5dadad2c0
toadder <zheka20042006@gmail.com>
2023-10-09 14:18:51 +0300

    Useless code was removed

--- src/utils/data/generateRandomNumber.ts
+++
@@ -1,4 +0,0 @@
-export const generateRandomNumber = (minNumber: number, maxNumber: number): number => {
-	const randomNumber: number = Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
-	return randomNumber;
-};
