7ac053b0f36599faae27c6ecd00efff6912ed705
toadder <zheka20042006@gmail.com>
2023-11-07 20:20:01 +0300

    Styles & config edits

--- next.config.js
+++ next.config.js
@@ -35,30 +35,6 @@ const nextConfig = {
 			}
 		]
 	},
-
-	async headers() {
-		return [
-			{
-				source: '/api/revalidate',
-				headers: [
-					{ key: 'Access-Control-Allow-Credentials', value: 'true' },
-					{
-						key: 'Access-Control-Allow-Origin',
-						value: process.env.WORDPRESS_SITE_URL
-					},
-					{
-						key: 'Access-Control-Allow-Methods',
-						value: 'GET'
-					},
-					{
-						key: 'Access-Control-Allow-Headers',
-						value:
-							'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Origin'
-					}
-				]
-			}
-		]
-	}
 }
 
 module.exports = withBundleAnalyzer(nextConfig)
